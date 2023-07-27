1e46789443cf0f0680a7d0a646267a1daf0df85a
Milianigia Rios <nanirios671@gmail.com>
2023-07-27 14:20:35 -0500

    July 27th JS and HTML lecture lesson and practice - nani rios

---
+++ js/external.js
@@ -0,0 +1,19 @@
+"use strict";
+
+console.log("Hey, this is me over in external js - You loaded me in with the src attribute");
+
+alert("hello, are you ready?");
+
+let cohortName= "Fenrir";
+
+alert("Welcome to the " + cohortName + " cohort!");
+
+/*prompt("is this your first time accessing this site? ");
+
+//let userAnswer
+userAnswer= prompt("Did you have a good lunch?");
+console.log("you siad that your lunch was "+ userAnswer);
+*/
+ let userResponse= confirm("We are learning javascript together");
+
+ alert("you said that it was " + userResponse + " that we ae learning javascript");
