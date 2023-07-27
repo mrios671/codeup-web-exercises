ec65429a545e81b57cc2694116514e0e3307b8cc
Milianigia Rios <nanirios671@gmail.com>
2023-07-27 16:07:07 -0500

    Here is my completed exercise for html and javascript from July 27th. -nani rios

--- js/external.js
+++ js/external.js
@@ -1,19 +1,81 @@
 "use strict";
 
-console.log("Hey, this is me over in external js - You loaded me in with the src attribute");
+ console.log("Hello from external JavaScript");
 
-alert("hello, are you ready?");
+        alert("Welcome to my website!");
 
-let cohortName= "Fenrir";
+    let userAnswer = prompt("What is your favorite color?");
 
-alert("Welcome to the " + cohortName + " cohort!");
+        alert("Wow! " + userAnswer + " is my favorite color too!");
 
-/*prompt("is this your first time accessing this site? ");
+//above is the first half of the html exercises from 7-27-23
 
-//let userAnswer
-userAnswer= prompt("Did you have a good lunch?");
-console.log("you siad that your lunch was "+ userAnswer);
-*/
- let userResponse= confirm("We are learning javascript together");
+//below is part one of the four prompts of the previous lesson's exercise.it is asking the user how ling they want to rent movies and giving them the total
+        alert("I heard that you liked movies...");
 
- alert("you said that it was " + userResponse + " that we ae learning javascript");
+    let ppd= 3;
+
+    let movieRentalQuestion = confirm("Did you know that our movie rentals are only $3 per day?");
+
+    let lmQuestion = prompt("How many days were you thinking of renting 'The Little mermaid'?");
+    let lmQuestionTotal = lmQuestion * ppd
+
+        alert("Great! To rent for " + lmQuestion + " days will only cost you " + (lmQuestionTotal) + " dollars!");
+
+    let bbQuestion = prompt("How many days were you thinking of renting 'Brother Bear'?");
+    let bbQuestionTotal = bbQuestion * ppd
+
+         alert("Great! To rent for " + bbQuestion + " days will only cost you " + (bbQuestionTotal) + " dollars!");
+
+    let hrcQuestion = prompt("How many days were you thinking of renting 'Hercules'?");
+    let hrcQuestionTotal = hrcQuestion * ppd
+
+         alert("Great! To rent for " + hrcQuestion + " days will only cost you " + (hrcQuestionTotal) + " dollars!");
+
+         alert("Given your provided answers, to rent all three movies will be a total of " + (lmQuestionTotal + bbQuestionTotal + hrcQuestionTotal) + " dollars.");
+
+//below is part two of the following lesson's exercise. it is asking for the users total pay for the week.
+
+        alert("Let's now go over how much you get paid to work (weird change of subject, I know...)");
+
+    let googleHours = prompt("How many hours do you work for Google per week?");
+    let googlePay = prompt("How much do you get paid per hour with Google?");
+
+    let googleTotal = googleHours * googlePay;
+
+        alert("Good for you, keep it up!");
+
+    let amazonHours = prompt("How many hours do you work for Amazon per week?");
+    let amazonPay = prompt("How much do you get paid per hour with Amazon?");
+
+    let amazonTotal = amazonHours * amazonPay;
+
+        alert("Great Job! Not everyone can do it like you.");
+
+    let facebookHours = prompt("How many hours do you work for Facebook per week?");
+    let facebookPay = prompt("How much do you get paid per hour with Facebook?");
+
+    let facebookTotal = facebookHours * facebookPay;
+
+        alert("Thats crazy!");
+
+    let weeklyPay = googleTotal + amazonTotal + facebookTotal
+
+        alert("Given your provided answers, your total pay for the week equates to $" + weeklyPay);
+
+//below is part three for the exercise from the previous lesson.
+
+    let roomInClass = true;
+    let fitInSchedule = false;
+    let canAttendClass = confirm(roomInClass && fitInSchedule)
+    console.log(canAttendClass);
+
+//Below is part four for the exercise from the previous lesson.
+
+    let moreThanTwo = true;
+    let premiumMember = false;
+    let offerIsValid = true;
+
+    let canHaveDiscount = confirm((moreThanTwo || premiumMember) && offerIsValid);
+    console.log(canHaveDiscount);
+
