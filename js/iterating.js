394e6b2554514c683e33d124b127ec1eb9c55711
Milianigia Rios <nanirios671@gmail.com>
2023-08-07 11:29:11 -0500

    finished a few more from the exercise. One more left to do.

--- js/iterating.js
+++ js/iterating.js
@@ -22,17 +22,28 @@
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
-
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
 
+    for(var i = 0; i < names.length; i++){
+        console.log("Our names are: " + names[i]);
+    }
+
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
+
+    names.forEach(function (element){
+        console.log(`The names of my siblings and I are: ${element}.`);
+    });
 
     /**
      * TODO:
