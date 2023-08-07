2ce6c50a3a9f9c3c23bdc399e371d0ca1d34006b
Milianigia Rios <nanirios671@gmail.com>
2023-08-07 11:33:36 -0500

    Finished with the exercise.

--- js/iterating.js
+++ js/iterating.js
@@ -59,4 +59,10 @@
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
 
+    let numArray = [1, 2, 3, 4, 5]
+
+    console.log("first = "+ numArray[0]);
+    console.log("second = "+ numArray[1]);
+    console.log("last = "+ numArray[4]);
+
 })();
