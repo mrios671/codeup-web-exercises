ba45159c89b5906ffdd90982e4473829399dd9df
Milianigia Rios <nanirios671@gmail.com>
2023-08-09 10:31:16 -0500

    Finished going over the first part of the exercise review.

--- js/objects.js
+++ js/objects.js
@@ -34,6 +34,12 @@
     }
     console.log(person.sayHello());
 
+    // the following is what we went over during review
+
+    person.sayHello = function(){
+        return `Hello my name is ${this.firstName} ${this.lastName}! `;
+    }
+    console.log(person.sayHello())
     /** TODO:
      * HEB has an offer for the shoppers that buy products amounting to
      * more than $200. If a shopper spends more than $200, they get a 12%
