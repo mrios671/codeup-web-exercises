1a50b5114c3dcf31eedc9c689ad9b6c3f49a4522
Milianigia Rios <nanirios671@gmail.com>
2023-08-16 09:12:09 -0500

    finished with the exercise. Going to do the bonus now

--- js/warmup.js
+++ js/warmup.js
@@ -42,3 +42,27 @@ console.log(logUser("ken2cool"));
 // }
 //
 // console.log(logUser());
+
+
+//------------- Break ----------------
+
+// topic: Object
+
+/**
+ * Wednesday -
+ *     Create an object literal with the following properties: a string property called “username” and a string property called “email” with string values representing the property name to them.
+ *     Assign your object literal into a variable called “userObject”. Console.log the objects properties to verify your values are assigned into the object properly.
+ * console.log(userObject.username) // “ken2cool”
+ * console.log(userObject.email) // “ken2cool@yahoo.com”
+ *     Bonus:
+ *         I. Refactor your logUser function to use your userObject’s property of username vs. an hardcoded string literal argument - update the string return from logUser to also include the userObject.email property e.g.: “ken2cool with email ken2cool@yahoo.com has logged in for the day.”
+ *         Ii. Refactor your userObject to have a userRoles property that is an array of strings listing the user’s roles - e.g “admin”, “moderator”, “buyer”, “seller” and other hypothetical roles a user could have on a website
+ *         Iii. Refactor your user object to have a nested userProfile object property - this nested property should have a userProfileUrl property with a string value mocking a link to an image. The other property should be a userLocation property with a string value mocking a CityName, StateName location the user could be from. Finally, add a property called numLikes with a number value indicating the users received ‘likes’ in our hypothetical application.
+ */
+
+let userObject = {
+    username : "ken2cool",
+    email: "ken2cool@yahoo.com"
+}
+console.log(userObject.username);
+console.log(userObject.email);
