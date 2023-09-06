758a1e4a4e5f59ee6b85b7d7f174e3890a9b1f55
Milianigia Rios <nanirios671@gmail.com>
2023-09-06 15:49:23 -0500

    pt.1 complete

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -10,12 +10,16 @@
 
 // $(".codeup").css("border", "1px solid red");
 
-$("li").css("font-size", "20px")
-// $("li").css("font-size", "20px").css("background-color", "springgreen");
-// $("h1").css("background-color", "springgreen");
-// $("p").css("background-color", "springgreen");
+// $("li").css("font-size", "20px")
+// // $("li").css("font-size", "20px").css("background-color", "springgreen");
+// // $("h1").css("background-color", "springgreen");
+// // $("p").css("background-color", "springgreen");
+//
+// let alertHeader = $("h1").html();
+// alert (alertHeader);
+//
+// $("li, h1, p").css("background-color", "springgreen");
 
-let alertHeader = $("h1").html();
-alert (alertHeader);
-
-$("li, h1, p").css("background-color", "springgreen");
+$("h1").on("click", function (){
+    $("h1").css("background-color", "springgreen");
+})
