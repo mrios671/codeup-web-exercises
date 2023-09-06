f2987b4e69fd61fa7d29ab5c32fc0e21940384a1
Milianigia Rios <nanirios671@gmail.com>
2023-09-06 14:47:00 -0500

    setup complete, ready to work on worksheet.

---
+++ js/dom-assessment-worksheet.js
@@ -0,0 +1,9 @@
+// TODO: Target the div with the id of deep-in-the-heart. When the div is clicked, spawn an alert that says "CLAPCLAPCLAPCLAPCLAP - Deep in the Heart of Texas!~".
+
+//TODO: Target the div with the id of what-color - when the div is clicked, the value of the color from that div should be populated as a string into the div#answer-color.
+
+// TODO: When the user clicks the button#tacoButton, the value typed into the input#tacoBoutIt will be populated into the span#favorite-taco. Our goal is for our user to be able to type what kind of tacos they like to get and see that value where "*replaceMe" is.
+
+// TODO: When a list item from the ul#list-states is hovered over, populate the span#insert-state with the value of the specific state-list-item hovered over to complete the sentence.
+
+//TODO: Five seconds after the page loads, insert a string with the message "Oh, hey, didn't see you there" into the div#five-seconds.
