2c0ad12e69512f2b362e89a0e49013b1351e3c2d
Milianigia Rios <nanirios671@gmail.com>
2023-09-06 13:44:05 -0500

    added and commented out previous code form exercise.

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -1,4 +1,17 @@
+// $(function (){
+//     alert("The page has finished loading!")
+// })
+
+// let contents = $("#header").html();
+// alert(contents);
+//
+// contents = $("#list").html();
+// alert(contents);
+
+// $(".codeup").css("border", "1px solid red");
+
 $("li").css("font-size", "20px")
+// $("li").css("font-size", "20px").css("background-color", "springgreen");
 // $("h1").css("background-color", "springgreen");
 // $("p").css("background-color", "springgreen");
 
