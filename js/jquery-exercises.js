44505c2103b1a01cc39f3902c41bf11dcf5dcb9e
Milianigia Rios <nanirios671@gmail.com>
2023-09-06 13:37:52 -0500

    finsihed with exercise

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -1,1 +1,8 @@
-$(".codeup").css("border", "1px solid red");
+$("li").css("font-size", "20px")
+// $("h1").css("background-color", "springgreen");
+// $("p").css("background-color", "springgreen");
+
+let alertHeader = $("h1").html();
+alert (alertHeader);
+
+$("li, h1, p").css("background-color", "springgreen");
