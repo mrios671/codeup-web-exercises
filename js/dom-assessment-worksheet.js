c3722f5f0e74dd3ee8e47e7b52852fee7105ee76
Milianigia Rios <nanirios671@gmail.com>
2023-09-06 20:50:29 -0500

    finished with the workseheet

--- js/dom-assessment-worksheet.js
+++ js/dom-assessment-worksheet.js
@@ -15,6 +15,29 @@ document.getElementById("what-color").addEventListener("click", function (){
 
 // TODO: When the user clicks the button#tacoButton, the value typed into the input#tacoBoutIt will be populated into the span#favorite-taco. Our goal is for our user to be able to type what kind of tacos they like to get and see that value where "*replaceMe" is.
 
+document.getElementById("tacoButton").addEventListener("click", function (){
+    let userInput = document.getElementById("tacoBoutIt").value;
+    document.getElementById("favorite-taco").textContent = userInput;
+})
+
 // TODO: When a list item from the ul#list-states is hovered over, populate the span#insert-state with the value of the specific state-list-item hovered over to complete the sentence.
 
+let listStates = document.getElementById("list-states");
+let insertState = document.getElementById("insert-state");
+
+listStates.addEventListener("mouseover", function (event){
+    if (event.target.classList.contains("state-list-item")) {
+        insertState.textContent = event.target.textContent;
+    }
+})
+
 //TODO: Five seconds after the page loads, insert a string with the message "Oh, hey, didn't see you there" into the div#five-seconds.
+
+function delayedMessage() {
+    let fiveSecDelay = document.getElementById("five-seconds");
+    fiveSecDelay.textContent = "Oh, hey, didn't see you there";
+}
+
+window.addEventListener("load", function (){
+    setTimeout(delayedMessage, 5000);
+})
