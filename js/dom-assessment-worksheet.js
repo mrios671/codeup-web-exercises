54b0654ecf102c5a1ebf700bf74e1daaa4f36570
Milianigia Rios <nanirios671@gmail.com>
2023-09-06 20:32:11 -0500

    a little less than halfway done.

--- js/dom-assessment-worksheet.js
+++ js/dom-assessment-worksheet.js
@@ -1,6 +1,17 @@
 // TODO: Target the div with the id of deep-in-the-heart. When the div is clicked, spawn an alert that says "CLAPCLAPCLAPCLAPCLAP - Deep in the Heart of Texas!~".
 
+let deepInTheHeart = function(event){
+    alert("CLAPCLAPCLAPCLAPCLAP - Deep in the Heart of Texas!~");
+}
+document.getElementById("deep-in-the-heart").addEventListener("click", deepInTheHeart);
+
+
 //TODO: Target the div with the id of what-color - when the div is clicked, the value of the color from that div should be populated as a string into the div#answer-color.
+
+document.getElementById("what-color").addEventListener("click", function (){
+    let color = document.getElementById ("what-color").style.color;
+    document.getElementById("answer-color").textContent = color;
+})
 
 // TODO: When the user clicks the button#tacoButton, the value typed into the input#tacoBoutIt will be populated into the span#favorite-taco. Our goal is for our user to be able to type what kind of tacos they like to get and see that value where "*replaceMe" is.
 
