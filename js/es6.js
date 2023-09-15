8ebe5c9c7820e03176a95a5a1b9dd89de7d891fe
Milianigia Rios <nanirios671@gmail.com>
2023-09-15 15:42:02 -0500

    finished with a few more, need to complete and edit a few more

--- js/es6.js
+++ js/es6.js
@@ -47,20 +47,20 @@ users.push({
 console.log(users)
 
 // TODO: replace `var` with `let` in the following variable declarations
-var emails = [];
-var names = [];
+let emails = [];
+let names = [];
 
 // TODO: rewrite the following using arrow functions
-users.forEach(function(user) {
+users.forEach((user) => {
     return emails.push(user.email);
 });
-users.forEach(function(user) {
+users.forEach((user) => {
     return names.push(user.name);
 });
 
 // TODO: replace `var` with `let` in the following declaration
-var developers = [];
-users.forEach(function(user) {
+let developers = [];
+users.forEach((user) => {
     // TODO: rewrite the code below to use object destructuring assignment
     //       note that you can also use destructuring assignment in the function
     //       parameter definition
@@ -73,12 +73,12 @@ users.forEach(function(user) {
 });
 
 // TODO: Use `let` for the following variable
-var list = '<ul>';
+let list = '<ul>';
 
 // TODO: rewrite the following loop to use a for..of loop
-developers.forEach(function (developer) {
+for (let developer of developers) {
 
     // TODO: rewrite the assignment below to use template strings
-    list += '<li>' + developer + '</li>';
+    list += `<li>${developer}</li>`;
 });
 list += '</ul>';
